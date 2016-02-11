d0f654463ebca7a362ee9282edc33f2367dc0e3e
Petr Marek <contact@petrmarek.eu>
2016-02-11 13:55:17 +0100

    dont reload on success; fix sizes

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -4,15 +4,34 @@
   });
 
   $.fn.activeAdminSortable = function() {
+    var wrap = this,
+        fixSortableSizes;
+
+    fixSortableSizes = function(wrap) {
+      wrap.find('td').width('auto').each(function(i, td) {
+        var $td;
+        $td = $(td);
+        $td.width($td.width());
+      });
+    };
+
+    $(window).on('resize', function(){ fixSortableSizes(wrap) });
+    fixSortableSizes(wrap);
+
     this.sortable({
       update: function(event, ui) {
-        var url = ui.item.find('[data-sort-url]').data('sort-url');
+        var url = ui.item.find('[data-sort-url]').data('sort-url'),
+            $tr = ui.item.parents('tbody').find('tr');
 
         $.ajax({
           url: url,
           type: 'post',
           data: { position: ui.item.index() + 1 },
-          success: function() { window.location.reload() }
+          success: function() {
+            $tr.removeClass('even odd');
+            $tr.filter(":even").addClass('odd');
+            $tr.filter(":odd").addClass('even');
+          }
         });
       }
     });
